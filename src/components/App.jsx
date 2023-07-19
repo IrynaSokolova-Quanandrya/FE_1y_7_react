d91fd8d59827000ac3cf9d33a8515b67a5a2f109
Iryna <irasokolova25@ukr.net>
2023-07-20 00:36:44 +0300

    add practice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,28 @@
 import painting from '../painting.json';
 import { Container } from './Container/Container';
 import { GalleryList } from './GalleryList/GalleryList';
-
+import { Recipe } from './Recipe/Recipe';
+import recipies from 'recipies';
 export const App = () => {
+  const {name, time, servings, calories, image} = recipies;
   return (
     <>
-    <Container title="Title">
+    {recipies.map(({name, time, servings, calories, image}) => (
+    <Recipe
+      key={image}
+      name={name}
+      time={time}
+      servings={servings}
+      calories={calories}
+      image={image}
+    />
+    ))
+    }
+    {/* <Container title="Title">
       <GalleryList items={painting}/>
     </Container>
     <Container>  </Container>
-    <Container title="Title2">  </Container>
+    <Container title="Title2">  </Container> */}
       
     </>
     
---
+++ src/components/Recipe/Recipe.jsx
@@ -0,0 +1,15 @@
+import { HiOutlineChartBar, HiOutlineChartPie } from 'react-icons/hi';
+import { BsAlarm } from 'react-icons/bs';
+import { RecipeInfo } from './RecipeInfo';
+
+export function Recipe ({name, time, servings, calories, image}){
+    return <div>
+        <h2>{name}</h2>
+        <img src={image} alt="" width="450" />
+        <ul>
+            <RecipeInfo text={`${time} min`} icon={BsAlarm}/>
+            <RecipeInfo text={`${servings} servings`} icon={HiOutlineChartPie}/>
+            <RecipeInfo text={`${calories} calories`} icon={HiOutlineChartBar}/>
+        </ul>
+    </div>
+}
