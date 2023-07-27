d15b88ffe8883bd40e2b2b59f42e724704ccfb46
Iryna <irasokolova25@ukr.net>
2023-07-27 19:43:26 +0300

    add colorPicker

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { ColorPicker } from 'components/ColorPicker/Colorpicker';
+import paintings from './paintings.json';
+import GalleryList from 'components/GalleryList/GalleryList';
+import { Container } from 'components/Container/Container';
+
+const colorPickerOptions = [
+  { label: 'red', color: '#F44336' },
+  { label: 'green', color: '#4CAF50' },
+  { label: 'blue', color: '#2196F3' },
+  { label: 'grey', color: '#607D8B' },
+  { label: 'pink', color: '#E91E63' },
+  { label: 'indigo', color: '#3F51B5' },
+];
+
+export default function App() {
+  return (    
+      <>   
+      <Container>
+          <ColorPicker options={colorPickerOptions}/>
+      </Container>
+        
+        <GalleryList items={paintings} />     
+      </>
+  )
+}
