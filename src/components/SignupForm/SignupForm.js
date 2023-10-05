319ddef9f71fa3be200555b7e4d3f313683fb209
Iryna <irasokolova25@ukr.net>
2023-10-05 16:13:44 +0300

    add files

---
+++ src/components/SignupForm/SignupForm.js
@@ -0,0 +1,43 @@
+import {Component} from 'react';
+import styles from './SignupForm.module.css';
+
+
+export class SignupForm extends Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
+  handleChange = evt => {
+    const { name, value } = evt.target;
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <form className={styles.form} autoComplete="off">
+        <label className={styles.label}>
+          <span>Пошта</span>
+          <input
+            type="email"
+            name="email"
+            onChange={this.handleChange}
+            value={this.state.email}
+          />
+        </label>
+
+<label className={styles.label}>
+  <span>Пароль</span>
+  <input
+    type="password"
+    name="password"
+    onChange={this.handleChange}
+    value={this.state.password}
+  />
+</label>
+
+        <button type="submit">Зареєструватися</button>
+      </form>
+    );
+  }
+}
