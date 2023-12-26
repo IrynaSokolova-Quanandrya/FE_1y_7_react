c0a9136149b16f6ba07d78fe34549ab6c4effca6
Iryna <irasokolova25@ukr.net>
2023-12-26 18:05:10 +0200

    add files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,45 +1,17 @@
-import { Component } from "react";
-import { GlobalStyle } from "Global.styled";
-import TodoList from "./TodoList/TodoList";
-import initialTodos from './TodoList/todos.json';
-import { TodoEditor } from "./TodoEditor/TodoEditor";
-import { nanoid } from "nanoid";
-/**
- * декілька форм та однаковий id
- * створюємо нову форму TodoEditor
- * додаємо нові завдання в стейт не мутуючи масиву
- * видаляємо завдання не мутуючи масиву
- * 
- * radio
- * чекбокси
- */
-export class App extends Component {
-  
-  state={
-    todos: initialTodos,
-  }
- 
-  addTodo = (text)=>{
-    const newTodo = {
-      id: nanoid(),
-      text,
-      completed: false
-    }
-    
-  this.setState((prevState)=>{
-    return {
-      todos: [newTodo, ...prevState.todos]
-    }
-  })
-  }
+import { Layout } from "./Tasks/Layout/Layout";
+import { AppBar } from "./Tasks/AppBar/AppBar";
+import { TaskForm } from "./Tasks/TaskForm/TaskForm";
+import { TaskList } from "./Tasks/TaskList/TaskList";
+import { useSelector } from "react-redux";
 
-  render(){
-    return (
-      <>
-      <TodoEditor addTodo={this.addTodo}/>
-      <TodoList todos={this.state.todos}/>
-      <GlobalStyle/>
-      </>
-    );
-  }
+export const App = () => {
+  const tasks = useSelector(state=>state.tasks)
+  console.log(tasks);
+  return (    
+    <Layout>
+      <AppBar />
+      <TaskForm />
+      <TaskList />
+    </Layout>
+  );
 };
