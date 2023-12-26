6cc6474cc8fc481373155e176570b18565a3a14f
Iryna <irasokolova25@ukr.net>
2023-12-26 20:38:21 +0200

    create store

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ import { TaskList } from "./Tasks/TaskList/TaskList";
 import { useSelector } from "react-redux";
 
 export const App = () => {
-  const tasks = useSelector(state=>state.tasks)
+  const tasks = useSelector((state)=>state.tasks[1]);
   console.log(tasks);
   return (    
     <Layout>
--- src/index.js
+++ src/index.js
@@ -1,30 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import { ThemeProvider } from 'styled-components';
 import { Provider} from "react-redux";
 import { store } from "./redux/store";
 
-
-const theme = {
-  colors:{
-    textColor: '#000',
-    bgColor: '#fff',
-    warning: 'orange',
-    success: 'green',
-    error: 'red',
-    grey: 'grey',
-    button: '#21a912',
-    accent: ' #7112ba'
-  }
-}
+console.log(store);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-     <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider> 
+      <App /> 
     </Provider>
   </React.StrictMode>
 );
