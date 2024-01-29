936e9106a3cfdac75dcbf12698bf39192ae4e3e9
Iryna <irasokolova25@ukr.net>
2024-01-29 16:32:52 +0200

    add files

--- src/index.js
+++ src/index.js
@@ -1,26 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import { ThemeProvider } from 'styled-components';
-
-
-const theme = {
-  colors:{
-    textColor: '#000',
-    bgColor: '#fff',
-    warning: 'orange',
-    success: 'green',
-    error: 'red',
-    grey: 'grey',
-    button: '#21a912',
-    accent: ' #7112ba'
-  }
-}
+import { Provider} from "react-redux";
+import { persistor, store } from "./redux/store";
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider> 
+    <Provider store={store}>
+      <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
+        <App /> 
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
